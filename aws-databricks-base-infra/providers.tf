4291407878e9137160b02ae970604f4aa20f0a8a
Syed Nijamuddin <syed.nijamuddin@version1.com>
2026-02-17 18:47:16 +0530

    adding databricks

--- aws-databricks-base-infra/providers.tf
+++ aws-databricks-base-infra/providers.tf
@@ -2,18 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+# Account-level provider (required for provisioning workspace)
 provider "databricks" {
   alias      = "mws"
   host       = "https://accounts.cloud.databricks.com"
   account_id = var.databricks_account_id
-
-  auth_type    = "aws"
-  aws_provider = aws
-}
-
-provider "databricks" {
-  host = databricks_mws_workspaces.this.workspace_url
-
-  auth_type    = "aws"
-  aws_provider = aws
+  auth_type  = "aws"
 }
